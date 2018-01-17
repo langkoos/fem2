430ae5a4b3ee5f9a38c5de1df8b031e12ab936b6
kainagel <nagel@vsp.tu-berlin.de>
2018-01-17 14:22:18 +1100

    adapt a bit towards that hawkesbury scenario.  note that this put a bunch of scenario adjustments into the RunMatsim method, which will have to be adapted or removed for more general useage of that method

--- src/main/java/org/matsim/run/RunMatsim.java
+++ src/main/java/org/matsim/run/RunMatsim.java
@@ -18,26 +18,40 @@
  * *********************************************************************** */
 package org.matsim.run;
 
+import org.apache.log4j.Logger;
 import org.matsim.api.core.v01.Scenario;
+import org.matsim.api.core.v01.network.Link;
+import org.matsim.api.core.v01.population.Activity;
+import org.matsim.api.core.v01.population.Leg;
+import org.matsim.api.core.v01.population.Person;
+import org.matsim.api.core.v01.population.PlanElement;
 import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
 import org.matsim.core.controler.Controler;
 import org.matsim.core.controler.OutputDirectoryHierarchy.OverwriteFileSetting;
+import org.matsim.core.network.NetworkUtils;
 import org.matsim.core.network.algorithms.NetworkCleaner;
 import org.matsim.core.network.algorithms.NetworkSimplifier;
 import org.matsim.core.scenario.ScenarioUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.matsim.core.network.NetworkUtils.*;
 
 /**
  * @author nagel
  *
  */
 public class RunMatsim {
+	private static final Logger log = Logger.getLogger(RunMatsim.class) ;
 
 	public static void main(String[] args) {
 		
 		Config config ;
 		if ( args==null || args.length==0 || args[0]=="" ) {
-			config = ConfigUtils.loadConfig( "workspace-csiro/proj1/wsconfig-for-matsim-v10.xml" ) ;
+//			config = ConfigUtils.loadConfig( "workspace-csiro/proj1/wsconfig-for-matsim-v10.xml" ) ;
+			config = ConfigUtils.loadConfig( "scenarios/hawkesbury-from-bdi-project-2018-01-16/config.xml" ) ;
 			config.controler().setLastIteration(1);
 			config.controler().setOverwriteFileSetting( OverwriteFileSetting.deleteDirectoryIfExists );
 		} else {
@@ -45,10 +59,48 @@ public class RunMatsim {
 		}
 		
 		Scenario scenario = ScenarioUtils.loadScenario(config) ;
-
+		
+		for ( Person person : scenario.getPopulation().getPersons().values() ) {
+			List<PlanElement> toRemove = new ArrayList<>() ;
+			boolean justRemoved = false ;
+			for ( PlanElement pe : person.getSelectedPlan().getPlanElements() ) {
+				if ( pe instanceof Activity ) {
+					final Activity act = (Activity) pe;
+					if ( act.getType().equals("Evacuation")) {
+						toRemove.add( act ) ;
+						justRemoved = true ;
+					}
+					act.setLinkId(null);
+				} else {
+					Leg leg = (Leg) pe;
+					if ( justRemoved ) {
+						justRemoved = false ;
+						toRemove.add(leg) ;
+					}
+					leg.setRoute(null);
+				}
+			}
+			person.getSelectedPlan().getPlanElements().removeAll( toRemove ) ;
+		}
+		
 		new NetworkCleaner().run(scenario.getNetwork());
 		new NetworkSimplifier().run(scenario.getNetwork());
-
+		new NetworkCleaner().run(scenario.getNetwork());
+		
+		for ( Link link : scenario.getNetwork().getLinks().values() ) {
+			double euclid = getEuclideanDistance(link.getFromNode().getCoord(), link.getToNode().getCoord());
+			if ( euclid > link.getLength() ) {
+				log.warn("linkId=" + link.getId() +  "; length=" + link.getLength()
+						+ "; EuclideanLength=" + euclid ) ;
+				link.setLength(euclid);
+			}
+			double maxSpeed = 100./3.6 ; // m/s
+			if ( link.getFreespeed() > maxSpeed ) {
+				log.warn("linkId=" + link.getId() + "; freespeed=" + link.getFreespeed() ) ;
+				link.setFreespeed(maxSpeed);
+			}
+		}
+		
 		Controler controler = new Controler( scenario ) ;
 
 		controler.run();
