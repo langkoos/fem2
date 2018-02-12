8ac3b7445e7776940c376e81d8e6ebe44e233081
kainagel <nagel@vsp.tu-berlin.de>
2018-02-12 16:06:02 +0800

    correcting a filename

--- src/test/java/femproto/demand/SubSectorsToPopulationTest.java
+++ src/test/java/femproto/demand/SubSectorsToPopulationTest.java
@@ -12,7 +12,7 @@ public class SubSectorsToPopulationTest {
 
 	@Test
 	public void readSubSectorsShapeFile() throws Exception {
-		final String networkFile = "test/input/scenarios/initial-2041-scenario/hn_net_ses_emme_2041_network.xml.gz" ;
+		final String networkFile = "scenarios/initial-2041-scenario/hn_net_ses_emme_2041_network.xml.gz" ;
 		final String inputShapeFile = utils.getPackageInputDirectory() + "hn_subsectors_2041_lower.shp" ;
 		final String evacNodesFile = utils.getPackageInputDirectory() + "2041_subsectors_safe_node_mapping.txt" ;
 		final String outputPopFile = utils.getOutputDirectory() + "pop.xml.gz" ;
