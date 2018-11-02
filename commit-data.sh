7cc0c72c89a5028c44cfeed01e2e7448612b7bdf
Pieter J. Fourie <fourie@ivt.baug.ethz.ch>
2018-11-02 12:24:34 +0800

    added a hack to change the log4j.xml file to our modified version

--- commit-data.sh
+++ commit-data.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
+mvn clean
 mvn package -DskipTests
-cp target/*-jar-with-dependencies.jar ./
+#couldnt get the maven packaging to overwrite log4j.xml so doing it manually
+jar -uf target/au-flood-evacuation-0.11.0-SNAPSHOT-jar-with-dependencies.jar -C src/main/resources log4j.xml
+#cp target/*-jar-with-dependencies.jar ./
 #pandoc -f markdown -t html -o README.html README.md
 #rsync -av scenarios/FEM2TestDataOctober18/clusterConfigs ../fem2-scenarios/
 #rsync -av scenarios/EIS_May2019/Scenario* ../fem2-scenarios/
 #rsync -av scenarios/FEM2TestDataOctober18/wma-flood-events ../fem2-scenarios/
-rsync -av au-flood-evacuation-0.11.0-SNAPSHOT-jar-with-dependencies.jar ../fem2-scenarios/
+rsync -av target/au-flood-evacuation-0.11.0-SNAPSHOT-jar-with-dependencies.jar ../fem2-scenarios/
 git log -n 1 > ../fem2-scenarios/commit-hash.txt
 #cd ../fem2-scenarios/
 #git add *
